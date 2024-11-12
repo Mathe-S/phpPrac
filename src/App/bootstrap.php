fb1f37fb56d329137c586e10f0d74759f13a66cc
Mathe-S <sharvadzemat@gmail.com>
2024-11-12 10:31:34 +0400

    feat: add csrfToken

--- src/App/bootstrap.php
+++ src/App/bootstrap.php
@@ -2,8 +2,8 @@
 
 declare(strict_types=1);
 
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+// error_reporting(E_ALL);
 
 require_once __DIR__ . "/../../vendor/autoload.php";
 
